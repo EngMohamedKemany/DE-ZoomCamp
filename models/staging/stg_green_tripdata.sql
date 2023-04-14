a768b8c8bd92f3a016f5a52e5b32c9af568fb7a0
eng.mohamed.kemany@gmail.com <eng.mohamed.kemany@gmail.com>
2023-04-14 11:18:45 +0000

    before dim_monthly_revenue

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,22 +1,30 @@
 {{ config(materialized='view') }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
+)
+
 select
     -- identifiers
+    {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
     
     -- timestamps
-    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
     store_and_fwd_flag,
     cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    -- yellow cabs are always street-hail
-    1 as trip_type,
+    cast(trip_type as integer) as trip_type,
     
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -24,17 +32,18 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(0 as numeric) as ehail_fee,
+    cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
 
-from {{ source('staging', 'yellow_tripdata') }}
-where vendorid is not NULL
+from tripdata
+where rn = 1
 
--- dbt build --m sgt_yellow_tripdata.sql --var 'is_test_run: false'
+-- dbt build --m sgt_green_tripdata.sql --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
   limit 100
